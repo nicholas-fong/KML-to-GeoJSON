3c32a5f555417f0f8c3cfb70789ce1ffc81ea6da
Nicholas Fong <nicholas.fong@gmail.com>
2023-09-18 19:45:15 -0700

    distance calculator

--- README.md
+++ README.md
@@ -5,31 +5,38 @@ KML data structure can range from relatively straight forward to complex schema 
 
 GeoJSON to KML:
 ```
-python3 geo2kml.py
-python2 kml2geo.py
+$python3 geo2kml.py
+$python3 kml2geo.py
 ```
 
 Using GDAL's ogr2ogr to do KML / GeoJSON conversions: <br>
 install gdal-bin: 
 ```
-sudo apt install gdal-bin
+$sudo apt install gdal-bin
 ```
 GeoJSON to KML conversion
 ```
-ogr2ogr -f 'KML' -a_srs EPSG:4326 fountains.kml fountains.geojson
+$python3 ogr2ogr -f 'KML' -a_srs EPSG:4326 fountains.kml fountains.geojson
 or
-ogr2ogr -f 'LIBKML' foundtains.kml fountains.geojson
+$ogr2ogr -f 'LIBKML' foundtains.kml fountains.geojson
 or
-ogr2ogr fountains.kml fountains.geojson
+$ogr2ogr fountains.kml fountains.geojson
 ```
 KML to GeoJSON conversion
 ```
-ogr2ogr fountains.geojson fountains.kml
+$ogr2ogr fountains.geojson fountains.kml
 ```
 GPX to KML conversion
 ```
-ogr2ogr -f 'LIBKML' -mapFieldType DateTime=String fountains.kml fountains.gpx
+$ogr2ogr -f 'LIBKML' -mapFieldType DateTime=String fountains.kml fountains.gpx
 (result is not very satisfactory, elevations in kml are lost)
-
-
 ```
+asc to GeoJSON and KML (see sample.asc)
+```
+$python3 asc2geokml.py sample
+```
+Calculate distance between two geolocations using haversine distance formula
+```
+$python3 distance.py
+```
+
---
+++ library/distance.py
@@ -0,0 +1,42 @@
+import math
+
+class HaversineCalculator:
+    EARTH_RADIUS = 6371000.0  # Earth's radius
+
+    @staticmethod
+    def haversine_distance(lat1, lon1, lat2, lon2):
+        d_lat = math.radians(lat2 - lat1)
+        d_lon = math.radians(lon2 - lon1)
+
+        a = math.sin(d_lat / 2) * math.sin(d_lat / 2) + \
+            math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * \
+            math.sin(d_lon / 2) * math.sin(d_lon / 2)
+
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+        return HaversineCalculator.EARTH_RADIUS * c  # unit in meters
+
+def main():
+    print("Calculate distance between 2 points on earth using haversine distance formula")
+    in_str1 = input("Enter first latitude and longitude separated by a comma: ")
+    print("You entered GPS", in_str1)
+    in_str2 = input("Enter second latitude and longitude separated by a comma: ")
+    print("You entered GPS", in_str2)
+
+    latlon1 = in_str1.strip().split(",")
+    lat1 = float(latlon1[0])
+    lon1 = float(latlon1[1])
+
+    latlon2 = in_str2.strip().split(",")
+    lat2 = float(latlon2[0])
+    lon2 = float(latlon2[1])
+
+    distance = HaversineCalculator.haversine_distance(lat1, lon1, lat2, lon2)
+
+    if distance < 1000:
+        print(f"Distance between two points = {distance:.1f} meters")
+    else:
+        print(f"Distance between two points = {distance / 1000:.1f} kilometers")
+
+if __name__ == "__main__":
+    main()
