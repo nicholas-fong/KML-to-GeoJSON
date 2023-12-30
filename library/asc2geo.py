1603751fd20bb4fd2e995c455e53bab4fee38a65
Nicholas Fong <nicholas.fong@gmail.com>
2023-12-30 11:32:01 -0800

    Update asc2geo.py

--- library/asc2geo.py
+++ library/asc2geo.py
@@ -1,38 +1,44 @@
-# input a CSV (asc) file and create corresponding GeoJSON file.
-# CSV (asc) file has 3 columns, latitude, longitude and name of the geo location (Point)
-# the third column (name field) can be quoted or unquoted
-# create a .geojson file
-# assume CSV data is WGS84 latitude and longitude
-# quotation marks in third column (name field) are optional and are filtered out, but don't use comma in this field.
-# leading and trailing blanks in name filed are also filtered out
-# additional columns beyond column 3 are ignored
-# Note: no elevation data, they are default to 0 
-
-import sys
 import csv
+import sys
 import json
-from geojson import FeatureCollection, Feature, Point
 
-if len(sys.argv) < 2:
-    print("Please enter a CSV file. File extension .asc is assumed") 
-    sys.exit(1)
+# Input and output file paths
+input_file_path = sys.argv[1] + '.asc'
+output_file_path = sys.argv[1] + '.geojson'
 
-basket = []             # empty basket to hold/collect features
-
-with open(sys.argv[1]+'.asc') as infile:
+# Read data from the ASCII file
+features = []
+with open(input_file_path) as infile:
     reader = csv.reader(infile, delimiter=',')
     for row in reader:
-        if row:         #extra if statement to capture empty lines
-            my_name=row[2].replace('"', '').strip()
+        if row:
             lat = float(row[0])
             lon = float(row[1])
-            elev = 0.0    
-            my_point=Point([lon,lat,elev])
-            basket.append(Feature(geometry=my_point, properties={"name":my_name}))
+            elev = 0
+            my_name = row[2].replace('"', '').strip()
 
-geo_string = json.dumps( FeatureCollection(basket), indent=2, ensure_ascii=False )
+            # Create a GeoJSON feature
+            feature = {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [lon, lat, elev]
+                },
+                "properties": {
+                    "name": my_name
+                }
+            }
 
-with open(sys.argv[1]+'.geojson', 'w') as outfile:
-    outfile.write( geo_string )
+            features.append(feature)
 
-print(geo_string)
+# Create a GeoJSON FeatureCollection
+geojson_data = {
+    "type": "FeatureCollection",
+    "features": features
+}
+
+# Write GeoJSON data to a file
+with open(output_file_path, 'w') as outfile:
+    json.dump(geojson_data, outfile, indent=2)
+
+#print(json.dumps(eval("geojson_data"), indent=2))
