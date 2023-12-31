4a8132c20b8d9c85c681b7a6a90d18849a880d7c
Nicholas Fong <nicholas.fong@gmail.com>
2023-12-30 23:04:03 -0800

    Update kml2geo.py

--- library/kml2geo.py
+++ library/kml2geo.py
@@ -1,76 +1,75 @@
+# For more complex KML structures, use GDAL's 'ogr2ogr' : sudo apt install gdal-bin
 import sys
 import xml.etree.ElementTree as ET
-from geojson import FeatureCollection, Feature, Point, LineString, Polygon, dumps
+from geojson import FeatureCollection, Feature, Point, LineString, Polygon
+import json
 
-# Define namespaces (KML uses kml namespaces and gx namespaces)
+# Define namespaces (KML uses kml_namespace and gx_namespace)
 kml_namespace = {'kml': 'http://www.opengis.net/kml/2.2'} 
 gx_namespace =  {'gx': 'http://www.google.com/kml/ext/2.2'}
 
-basket=[]
+features=[]
 
 # function to extract coordinates from a geometry element, returns an array of tuples
 def extract_coordinates(geometry_element):
-    coordinates_elem = geometry_element.find('kml:coordinates', namespaces=kml_namespace)
+    coordinates_elem = geometry_element.find('kml:coordinates', kml_namespace)
     if coordinates_elem is not None:
         # coordinates is a list of strings
         coordinates = coordinates_elem.text.strip().split()
         # convert to a list of tuples
         list_of_tuples = [tuple(map(float, item.split(','))) for item in coordinates]
         return list_of_tuples
-
-# main()
-# For more complex KML files, use GDAL's 'ogr2ogr' : sudo apt install gdal-bin
-
+    
 with open(sys.argv[1]+".kml") as infile:
     tree = ET.parse(infile)
 root = tree.getroot()
 
-# Iterate through Placemark elements
-for placemark in root.findall('.//kml:Placemark', namespaces=kml_namespace):
-    name_elem = placemark.find('kml:name', namespaces=kml_namespace)
+# Find all placemark elements
+for placemark in root.findall('.//kml:Placemark', kml_namespace):
+    name_elem = placemark.find('kml:name', kml_namespace)
     name = name_elem.text.strip() if name_elem is not None else 'Unnamed'
-    time_elem = placemark.find('.//kml:TimeStamp/kml:when', namespaces=kml_namespace)
+    time_elem = placemark.find('kml:TimeStamp/kml:when', kml_namespace)
     time_stamp = time_elem.text.strip() if time_elem is not None else None
 
     # Check for geometry types: Point, LineString, or Polygon
-    point = placemark.find('.//kml:Point', namespaces=kml_namespace)
-    line_string = placemark.find('.//kml:LineString', namespaces=kml_namespace)
-    polygon = placemark.find('.//kml:Polygon', namespaces=kml_namespace)
+    point = placemark.find('kml:Point', kml_namespace)
+    line_string = placemark.find('kml:LineString', kml_namespace)
+    polygon = placemark.find('kml:Polygon', kml_namespace)
 
     if point:
         my_point = extract_coordinates(point)
-        basket.append(Feature(geometry=Point(my_point[0]), properties={"name":name}))
+        features.append(Feature(geometry=Point(my_point[0]), properties={"name":name}))
 
     elif line_string:
         my_line = extract_coordinates(line_string)
-        basket.append(Feature(geometry=LineString(my_line),properties={"name":name,"timestamp":time_stamp} )) 
+        features.append(Feature(geometry=LineString(my_line),properties={"name":name,"timestamp":time_stamp} )) 
 
     elif polygon:
         my_rings = []
-        outer_ring = polygon.find('.//kml:outerBoundaryIs/kml:LinearRing', namespaces=kml_namespace)
+        outer_ring = polygon.find('kml:outerBoundaryIs/kml:LinearRing', kml_namespace)
         my_rings.append(extract_coordinates(outer_ring))
         
-        inner_rings = polygon.findall('.//kml:innerBoundaryIs/kml:LinearRing', namespaces=kml_namespace)
+        inner_rings = polygon.findall('kml:innerBoundaryIs/kml:LinearRing', kml_namespace)
         for inner_ring_elem in inner_rings:                
             my_rings.append(extract_coordinates(inner_ring_elem))
-        basket.append(Feature(geometry=Polygon(my_rings),properties={"name":name})) 
+        features.append(Feature(geometry=Polygon(my_rings),properties={"name":name})) 
 
-    # Look for <gx:Track> <gx:coord>
-    gx_track = placemark.find("{http://www.google.com/kml/ext/2.2}Track")
-    if gx_track is not None:
-        gx_coord = gx_track.findall("{http://www.google.com/kml/ext/2.2}coord")  #<gx:coord>
-        list_coords=[]
-        for coord in gx_coord:
-            my_xyz = list(map(float, coord.text.split()))  # coord is a list of 3 floats: longitude, latitude, altitude
-            list_coords.append(my_xyz)  # collect all the track points
-        list_tuples = [tuple(lst) for lst in list_coords]  # convert list of xyz to list of tuples before invoking constructor    
-        basket.append(Feature(geometry=LineString(list_tuples),properties={"name":name,"timestamp":time_stamp} ))
+    # Look in the placemark for <gx:Track>
+    gx_track = placemark.find('gx:Track', gx_namespace)
+    if gx_track:
+        gx_coordinates = gx_track.findall('gx:coord', gx_namespace)
+        coordinate_list=[]
+        for lonlat in gx_coordinates:
+            xyz = list(map(float, lonlat.text.split()))  # a list of 3 floats: longitude, latitude, elevation
+            coordinate_list.append(xyz)  # collect all the gx:Track points
+        list_tuples = [tuple(lst) for lst in coordinate_list]  # convert list of floats to list of tuples, feed LineString constructor    
+        features.append(Feature(geometry=LineString(list_tuples),properties={"name":name,"timestamp":time_stamp} ))
 
-geojson_string = dumps(FeatureCollection(basket), indent=2, ensure_ascii=False)
+geojson_string = json.dumps(FeatureCollection(features), indent=2, ensure_ascii=False)
 # defaults to multi-line human-readable geojson output
-# comment out above line, uncomment below line to do one-line geojson output, to save space.
-# geojson_string = dumps(FeatureCollection(basket), ensure_ascii=False)
+# if one-line geojson is desired, comment out above line, uncomment below line.
+# geojson_string = json.dumps(FeatureCollection(features), ensure_ascii=False)
 
-print(geojson_string)
+#print(geojson_string)
 with open(sys.argv[1]+'.geojson', 'w') as outfile:
     outfile.write( geojson_string )
