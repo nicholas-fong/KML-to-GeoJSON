b379ad10e07ddb8609a32349c8a7e8908be40036
Nicholas Fong <nicholas.fong@gmail.com>
2024-01-04 14:48:56 -0800

    Update asc2wpt.py

--- library/asc2wpt.py
+++ library/asc2wpt.py
@@ -2,24 +2,27 @@
 
 import sys
 import csv
-import gpxpy
+import gpxpy.gpx as hiking
+
+gpx = hiking.GPX()                   #create a new GPX object called gpx
 
 if len(sys.argv) < 2:
     print("Please enter a CSV file. File extension .asc is assumed") 
     sys.exit(1)
 
-new = gpxpy.gpx.GPX()
-
 with open(sys.argv[1]+'.asc') as f:
     reader = csv.reader(f, delimiter=',', quotechar='"')
     for row in reader:
         if row:         # an extra statement to filter out empty lines
-            wpt = gpxpy.gpx.GPXWaypoint( float(row[0]), float(row[1]) )
-            wpt.name = row[2].replace('"', '').strip()
+            wpt = hiking.GPXWaypoint( float(row[0]), float(row[1]) )
+            try:
+                wpt.name = row[2].replace('"', '').strip()
+            except:
+                wpt.name = None    
             wpt.elevation = 0.0            
-            new.waypoints.append(wpt)
+            gpx.waypoints.append(wpt)
+
+#print( gpx.to_xml())
 
 with open(sys.argv[1]+'.gpx', 'w') as outfile:
-    outfile.write( new.to_xml() )
-
-print( new.to_xml())
+    outfile.write( gpx.to_xml() )
