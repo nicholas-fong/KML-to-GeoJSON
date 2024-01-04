aac029e915910cc412506bb4637bdb6aab39ae13
Nicholas Fong <nicholas.fong@gmail.com>
2024-01-04 14:47:31 -0800

    Update asc2rte.py

--- library/asc2rte.py
+++ library/asc2rte.py
@@ -2,26 +2,25 @@
 
 import sys
 import csv
-import gpxpy
-import gpxpy.gpx
+import gpxpy.gpx as hiking
 
-gpx = gpxpy.gpx.GPX()                   #create a new GPX object
+gpx = hiking.GPX()                   #create a new GPX object called gpx
 
 route_name = input ("What do you want to call the route ?  ") 
 
 # Create a route segment
-route = gpxpy.gpx.GPXRoute(route_name) 
+route = hiking.GPXRoute(route_name) 
 gpx.routes.append(route)  
 
 with open(sys.argv[1]+'.asc' ) as f:
     readline = csv.reader(f, delimiter=',', quotechar='"')
-    for i in readline:
-        if i:           # Create a route point
-            route_point = gpxpy.gpx.GPXRoutePoint(latitude=float(i[0]),longitude=float(i[1]))
+    for row in readline:
+        if row:           # Create a route point
+            route_point = hiking.GPXRoutePoint(latitude=float(row[0]),longitude=float(row[1]))
             route_point.elevation = 0.0
             route.points.append(route_point)  # Add the route point to the route
 
-print( gpx.to_xml())
+#print( gpx.to_xml())
 
 with open(sys.argv[1]+'.gpx', 'w') as outfile:
     outfile.write( gpx.to_xml() )
