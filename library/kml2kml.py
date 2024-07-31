43a940aa06e01a96d8c1cb8000e9c79787efb069
Nicholas Fong <nicholas.fong@gmail.com>
2024-07-31 11:35:44 -0700

    Update kml2kml.py

--- library/kml2kml.py
+++ library/kml2kml.py
@@ -4,7 +4,6 @@ import sys
 
 kml_namespace = {'kml': 'http://www.opengis.net/kml/2.2'}
 
-# style 1 of pretty print, eliminat blank lines, returns regular string
 def prettify(element):
     rough_string = ET.tostring(element, encoding='utf-8', xml_declaration=True)
     reparsed = minidom.parseString(rough_string)
@@ -12,24 +11,12 @@ def prettify(element):
     # Remove extra blank lines
     lines = [line for line in pretty_string.split('\n') if line.strip()]
     return '\n'.join(lines)
-# style 2 of pretty print: ET.tosring --> minidom --> toprettyxml, returns binary string
-# def prettify(element):
-#   rough_string = ET.tostring(element, encoding='utf-8', xml_declaration=True)
-#   reparsed = minidom.parseString(rough_string)
-#   return reparsed.toprettyxml(indent="  ", encoding='utf-8')
-# style 3 KML to string pretty print
-# print( ET.tostring(kml, encoding='utf-8', pretty_print=True, xml_declaration=True ).decode() )
 
-# main()
 with open(sys.argv[1] + ".kml") as infile:
     tree = ET.parse(infile)
 root = tree.getroot()
 
-# sylte 1 output: <?xml version="1.0" encoding="utf-8"?>
 pretty_kml = prettify(root)
-#print (pretty_kml)
-with open(sys.argv[1]+'.kml', 'w') as output_file:
-    output_file.write(pretty_kml)
-# style 2 output: <?xml version='1.0' encoding='UTF-8'?>
-# Save the KML element
-# tree.write(sys.argv[1] + ".kml", encoding="utf-8", xml_declaration=True)
+with open(sys.argv[1]+'.kml', 'w') as outfile:
+    outfile.write(pretty_kml)
+print ( f"File saved as {sys.argv[1]+'kml'}")    
